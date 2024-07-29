5e5e43b11d149a9b5354b08432e3f3a7cd5d4287
u9g <git@u9g.dev>
2024-07-28 23:02:12 -0400

    use onGameMessage for ChatMessageReceivedEvent

--- src/client/java/dev/u9g/mixin/client/events/ClientPlayNetworkHandlerMixin.java
+++ src/client/java/dev/u9g/mixin/client/events/ClientPlayNetworkHandlerMixin.java
@@ -1,10 +1,8 @@
 package dev.u9g.mixin.client.events;
 
-import dev.u9g.events.PlaySoundCallback;
-import dev.u9g.events.PlaySoundEvent;
-import dev.u9g.events.TitleTextCallback;
-import dev.u9g.events.TitleTextEvent;
+import dev.u9g.events.*;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
 import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,6 +12,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
+    @Inject(at = @At("HEAD"), method = "onGameMessage", cancellable = true)
+    private void skyplus$onChat(GameMessageS2CPacket packet, CallbackInfo ci) {
+        ChatMessageReceivedEvent cmre = new ChatMessageReceivedEvent(packet.content(), packet.content().getString(), false);
+        ChatMessageReceivedCallback.getEvent().invoker().invoke(cmre);
+
+        if (cmre.isCancelled()) {
+            ci.cancel();
+        }
+    }
+
     @Inject(at = @At("HEAD"), method = "onTitle", cancellable = true)
     private void skyplus$onTitle(TitleS2CPacket packet, CallbackInfo ci) {
         TitleTextEvent tte = new TitleTextEvent(packet.getTitle(), packet.getTitle().getString(), false);
@@ -27,7 +35,7 @@ public class ClientPlayNetworkHandlerMixin {
     // packet.sound.getKeyOrValue().left().value.value.path.equals("entity.bat.takeoff")
 
     @Inject(at = @At("HEAD"), method = "onPlaySound", cancellable = true)
-    private void skyplus$onTitle(PlaySoundS2CPacket packet, CallbackInfo ci) {
+    private void skyplus$onSound(PlaySoundS2CPacket packet, CallbackInfo ci) {
         PlaySoundEvent pse = new PlaySoundEvent(packet.getSound().getKeyOrValue().left().map(x -> x.getValue().getPath()).orElse(null), false);
         PlaySoundCallback.getEvent().invoker().invoke(pse);
 
