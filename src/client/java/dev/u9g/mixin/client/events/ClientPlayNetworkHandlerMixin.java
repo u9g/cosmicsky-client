d65cb33faeadaca2c310a57d5b4d1730a41774ef
u9g <git@u9g.dev>
2024-07-29 00:34:25 -0400

    all events that give text should have a msg param that is unstyled txt + add subtitle event

--- src/client/java/dev/u9g/mixin/client/events/ClientPlayNetworkHandlerMixin.java
+++ src/client/java/dev/u9g/mixin/client/events/ClientPlayNetworkHandlerMixin.java
@@ -1,20 +1,26 @@
 package dev.u9g.mixin.client.events;
 
 import dev.u9g.events.*;
+import kotlin.text.Regex;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.network.packet.s2c.play.SubtitleS2CPacket;
 import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
+    @Unique
+    Regex r = new Regex("§.");
+
     @Inject(at = @At("HEAD"), method = "onGameMessage", cancellable = true)
     private void skyplus$onChat(GameMessageS2CPacket packet, CallbackInfo ci) {
-        ChatMessageReceivedEvent cmre = new ChatMessageReceivedEvent(packet.content(), packet.content().getString(), false);
+        ChatMessageReceivedEvent cmre = new ChatMessageReceivedEvent(packet.content(), r.replace(packet.content().getString(), ""), false);
         ChatMessageReceivedCallback.getEvent().invoker().invoke(cmre);
 
         if (cmre.isCancelled()) {
@@ -24,8 +30,18 @@ public class ClientPlayNetworkHandlerMixin {
 
     @Inject(at = @At("HEAD"), method = "onTitle", cancellable = true)
     private void skyplus$onTitle(TitleS2CPacket packet, CallbackInfo ci) {
-        TitleTextEvent tte = new TitleTextEvent(packet.getTitle(), packet.getTitle().getString(), false);
+        TitleTextEvent tte = new TitleTextEvent(packet.getTitle(), r.replace(packet.getTitle().getString(), ""), false);
         TitleTextCallback.getEvent().invoker().invoke(tte);
+
+        if (tte.component3()) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "onSubtitle", cancellable = true)
+    private void skyplus$onSubtitle(SubtitleS2CPacket packet, CallbackInfo ci) {
+        SubtitleTextEvent tte = new SubtitleTextEvent(packet.getSubtitle(), r.replace(packet.getSubtitle().getString(), ""), false);
+        SubtitleTextCallback.getEvent().invoker().invoke(tte);
 
         if (tte.component3()) {
             ci.cancel();
