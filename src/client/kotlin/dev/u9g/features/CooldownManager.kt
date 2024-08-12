13eb5861381217d517959c7107b644deaea74ad1
u9g <git@u9g.dev>
2024-08-12 11:26:34 -0400

    add direwolf to cooldown hud

--- src/client/kotlin/dev/u9g/features/CooldownManager.kt
+++ src/client/kotlin/dev/u9g/features/CooldownManager.kt
@@ -13,6 +13,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.nbt.StringNbtReader
 import java.io.File
 import java.nio.file.Path
 import java.util.concurrent.TimeUnit
@@ -33,8 +34,10 @@ fun formatTime(seconds: Long): String {
     return stringBuilder.toString()
 }
 
+// PET: Dire Wolf [Dire Wolf Buff (2x Mobs Killed per Stack [35s])]
+
 class Cooldown(
-    private val cooldownInSeconds: Int,
+    var cooldownInSeconds: Int,
     val icon: ItemStack,
     val command: String,
     private val colorString: String
@@ -43,7 +46,7 @@ class Cooldown(
 
     fun color() = "§" + this.colorString
 
-    fun timeStr(): String {
+    open fun timeStr(): String {
         val timeSinceLastUsed = if (MinecraftClient.getInstance().currentScreen is MoveHudOnScreen) {
             System.currentTimeMillis() - (this.cooldownInSeconds * 1000) / 2
         } else {
@@ -65,13 +68,21 @@ data class CooldownSettings(
 val SETTINGS_FILE_PATH: Path = File("cooldown_hud_settings.json").toPath()
 
 object CooldownManager {
+    private val DIRE_HEAD = ItemStack(Items.PLAYER_HEAD).also {
+        it.nbt =
+            StringNbtReader.parse("{SkullOwner:{Id:[I;1510996716,1410484424,-1530203921,-1510562530],Name:\"dire_wolf\",Properties:{textures:[{Value:\"eyJ0aW1lc3RhbXAiOjE1NjMyNzg4MTM0OTksInByb2ZpbGVJZCI6IjdjODk1YWQwMTFkMDQzNTA5YWU1ZjJiYjFjZjZjOGVhIiwicHJvZmlsZU5hbWUiOiJCYXNpY0JBRSIsInRleHR1cmVzIjp7IlNLSU4iOnsidXJsIjoiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9iNTUyMGNmNTExOTg3YTkzYjAxOGY4MzA4N2U4ZGI0Y2JlODNjZjlkZTdkZTg4MzQ5NDViODJjMDczN2JlM2YyIn19fQ==\"}]}}}")
+    }
+
     private val cooldowns = mapOf(
         "heal" to Cooldown(300, ItemStack(Items.GLISTERING_MELON_SLICE), "/heal", "e"),
         "eat" to Cooldown(300, ItemStack(Items.COOKED_BEEF), "/eat", "a"),
         "fix" to Cooldown(2 * 60, ItemStack(Items.ANVIL), "/fix", "b"),
         "near" to Cooldown(30, ItemStack(Items.COMPASS), "/near", "c"),
-        "bleed" to Cooldown(3 * 60, ItemStack(Items.GOLDEN_SWORD), "Bleed", "f")
+        "bleed" to Cooldown(3 * 60, ItemStack(Items.GOLDEN_SWORD), "Bleed", "f"),
+        "dire" to Cooldown(1111111, DIRE_HEAD, "Dire", "b")
     )
+
+    private val DIRE_REGEX = "PET: Dire Wolf \\[Dire Wolf Buff \\(\\dx Mobs Killed per Stack \\[(\\d+)s]\\)]".toRegex()
 
     var settings: CooldownSettings
 
@@ -240,6 +251,13 @@ object CooldownManager {
 
                 "Appetite has been satiated." -> {
                     cooldowns["eat"]?.timeSinceLastUsed = System.currentTimeMillis()
+                }
+            }
+
+            if (DIRE_REGEX.matches(event.msg)) {
+                DIRE_REGEX.find(event.msg)?.groups?.get(1)?.let {
+                    cooldowns["dire"]?.cooldownInSeconds = it.value.toInt()
+                    cooldowns["dire"]?.timeSinceLastUsed = System.currentTimeMillis()
                 }
             }
 
