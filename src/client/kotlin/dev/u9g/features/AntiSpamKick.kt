cc5a3e34c3c3d2606f0504f3b001a36bf6b85dbb
u9g <git@u9g.dev>
2024-08-22 11:37:32 -0400

    add better antispamkick

--- src/client/kotlin/dev/u9g/features/AntiSpamKick.kt
+++ src/client/kotlin/dev/u9g/features/AntiSpamKick.kt
@@ -1,19 +1,23 @@
 package dev.u9g.features
 
+import dev.u9g.events.MsgOrCmd
 import dev.u9g.events.UserSendMsgOrCmdCallback
 import dev.u9g.mc
 import net.minecraft.text.Text
 
 object AntiSpamKick {
+    private val COMMANDS_THAT_TRIGGER_ANTISPAM = "^((m)|(msg)|(w)|(whisper)|(t)|(tell)|(r)) .+".toRegex()
     private var lastMessageAt = 0L
 
     init {
         UserSendMsgOrCmdCallback.event.register {
-            if (System.currentTimeMillis() - lastMessageAt < 3000L) {
-                it.isCancelled = true
-                mc.player?.sendMessage(Text.of("§b§l(!) SkyPlus -> §r§fPrevented you from getting kicked from spam."))
-            } else {
-                lastMessageAt = System.currentTimeMillis()
+            if (it.type == MsgOrCmd.MSG || (it.type == MsgOrCmd.CMD && COMMANDS_THAT_TRIGGER_ANTISPAM.matches(it.msg))) {
+                if (System.currentTimeMillis() - lastMessageAt < 3000L) {
+                    it.isCancelled = true
+                    mc.player?.sendMessage(Text.of("§b§l(!) SkyPlus -> §r§fPrevented you from getting kicked from spam."))
+                } else {
+                    lastMessageAt = System.currentTimeMillis()
+                }
             }
         }
     }
